fef34ce8a48c0e148cff0d9709d95c071688f038
Shaygan Shokrollahi <shayganshok0@gmail.com>
2017-11-07 19:09:02 +0330

    Debugging...

--- ip.js
+++ ip.js
@@ -3,7 +3,7 @@ var IP = $.getJSON('//piorra.ir/sh_sh_dev/github/ip.php',{},function(data){
     console.log(data);
 });
 function setLanguageByIP() {
-    if (IP.responseJSON.Country == "ir") {
+    if (IP.Country == "IR") {
         if (l !== "fa") {
             var CHL = confirm("کشور شما ایران شناسایی شد ، آیا مایل هستید زبان را به فارسی تغییر دهید؟");
             if (CHL) {
