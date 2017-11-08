e3e9e3576b659b57518e86d7cdce48d54e315671
Shaygan Shokrollahi <shayganshok0@gmail.com>
2017-11-08 21:09:49 +0330

    Language Optimization

--- ip.js
+++ ip.js
@@ -1,15 +1,18 @@
-var IP = $.getJSON('//piorra.ir/sh_sh_dev/github/ip.php',{},function(data){
+var IP = $.getJSON('//github.piorra.ir/ghp/sh-sh-dev/ip',{},function(data){
     setLanguageByIP();
-    console.log(data);
 });
 function setLanguageByIP() {
-    if (IP.Country == "IR") {
-        if (l !== "fa") {
+    if (IP.responseJSON.Country == "IR") {
+        if (l !== "fa" && getCookie("chlbyip") != "false") {
             var CHL = confirm("کشور شما ایران شناسایی شد ، آیا مایل هستید زبان را به فارسی تغییر دهید؟");
             if (CHL) {
                 setCookie("language","fa",365);
+                setCookie("chlbyip","false",7);
                 l = "fa";
                 location.reload();
+            }
+            else {
+                setCookie("chlbyip","false",365);
             }
         }
     }
